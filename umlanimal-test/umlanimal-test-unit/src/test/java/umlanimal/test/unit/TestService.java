afa732c73074b3672b3e9c9622fb567f83f3ff2a
ninaodelius <nina.odelius@gmail.com>
2022-01-20 12:57:27 +0100

    add spring beans TestService

--- umlanimal-test/umlanimal-test-unit/src/test/java/umlanimal/test/unit/TestService.java
+++ umlanimal-test/umlanimal-test-unit/src/test/java/umlanimal/test/unit/TestService.java
@@ -1,15 +1,27 @@
 package umlanimal.test.unit;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import umlanimal.domain.Owl;
+import umlanimal.service.ForestService;
 
 public class TestService {
 
     ApplicationContext applicationContext= new ClassPathXmlApplicationContext("classpath:umlanimal-service.xml");
 
+    private ForestService forestService;
+
     @Before
     public void Before(){
-        applicationContext.getBean("forestService");
+        forestService = (ForestService) applicationContext.getBean("forestService");
+    }
+
+    @Test
+    public void testForestServiceDependencyInjection(){
+        Owl owl = forestService.createOwl(95);
+        Assert.assertNotNull(owl);
     }
 }
